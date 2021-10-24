c9093ec7acc4122600e90e299381be19027a7697
naoya-3333 <2111141003f@ed.fuk.kindai.ac.jp>
2021-10-24 05:38:09 +0000

    カートに入れる機能

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,4 +1,18 @@
 class CartItemsController < ApplicationController
   def new
+    @product = Product.find(params[:product_id])
+    @cartitem = CartItem.new
+  end
+  
+  def create 
+    @cartitem = CartItem.create(product_id: params[:cart_item][:product_id], 
+    cart_id: current_cart.id ,
+    qty: params[:cart_item][:qty])
+    
+    if @cartitem.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'products#index'
   resources :products
+  resources :cart_items, only: [:new, :create, :destroy]
+  resources :carts, only: [:show]
+  
 end
